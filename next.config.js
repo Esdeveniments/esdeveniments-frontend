ce284bda413a3004ed1b8cc992517ee6e7b41409
Albert Olivé <albertolivecorbella@gmail.com>
2025-04-28 07:26:39 +0200

    wip

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-import { withSentryConfig } from '@sentry/nextjs';
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,36 +6,42 @@ const nextConfig = {
     scrollRestoration: true,
   },
   productionBrowserSourceMaps: true,
-  i18n: {
-    locales: ['ca-ES'],
-    defaultLocale: 'ca-ES',
-  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
   headers: async () => [
     {
-      source: '/:path*',
+      source: "/:path*",
       headers: [
-        { key: 'X-DNS-Prefetch-Control', value: 'on' },
-        { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-        { key: 'X-XSS-Protection', value: '1; mode=block' },
-        { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-        { key: 'X-Content-Type-Options', value: 'nosniff' },
-        { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+        { key: "X-DNS-Prefetch-Control", value: "on" },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=63072000; includeSubDomains; preload",
+        },
+        { key: "X-XSS-Protection", value: "1; mode=block" },
+        { key: "X-Frame-Options", value: "SAMEORIGIN" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "Referrer-Policy", value: "origin-when-cross-origin" },
       ],
     },
     {
-      source: '/api/:path*',
+      source: "/api/:path*",
       headers: [
-        { key: 'Access-Control-Allow-Credentials', value: 'true' },
-        { key: 'Access-Control-Allow-Origin', value: '*' },
-        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-        { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
       ],
     },
   ],
@@ -54,7 +59,4 @@ const sentryWebpackPluginOptions = {
   automaticVercelMonitors: true,
 };
 
-export default withSentryConfig(
-  withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(nextConfig),
-  sentryWebpackPluginOptions
-);
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
