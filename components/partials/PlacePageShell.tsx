32b5886b0e2f159d6b19900e84f344c21d650b44
Albert Olivé <albertolivecorbella@gmail.com>
2025-11-15 10:07:00 +0100

    chore: wip

--- components/partials/PlacePageShell.tsx
+++ components/partials/PlacePageShell.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import HybridEventsList from "@components/ui/hybridEventsList";
 import ClientInteractiveLayer from "@components/ui/clientInteractiveLayer";
 import type { PageData, PlaceTypeAndLabel, JsonLdScript } from "types/common";
@@ -38,18 +39,21 @@ export default function PlacePageShell({
         <JsonLdServer key={s.id} id={s.id} data={s.data} />
       ))}
 
-      <HybridEventsList
-        initialEvents={initialEvents}
-        placeTypeLabel={placeTypeLabel}
-        pageData={pageData}
-        noEventsFound={noEventsFound}
-        place={place}
-        category={category}
-        date={date}
-        serverHasMore={serverHasMore}
-        hasNews={hasNews}
-        categories={categories}
-      />
+      {/* Wrapped in Suspense because HybridEventsList contains components using useSearchParams() */}
+      <Suspense fallback={null}>
+        <HybridEventsList
+          initialEvents={initialEvents}
+          placeTypeLabel={placeTypeLabel}
+          pageData={pageData}
+          noEventsFound={noEventsFound}
+          place={place}
+          category={category}
+          date={date}
+          serverHasMore={serverHasMore}
+          hasNews={hasNews}
+          categories={categories}
+        />
+      </Suspense>
 
       <ClientInteractiveLayer
         categories={categories}
