4ce909d9b52054ea85cfec2761a839bdea7fd954
Albert Olivé <albertolivecorbella@gmail.com>
2025-11-19 14:09:09 +0100

    chore: wip

--- components/ui/common/skeletons/SearchSkeleton.tsx
+++ components/ui/common/skeletons/SearchSkeleton.tsx
@@ -4,7 +4,7 @@ export default function SearchSkeleton() {
       className="w-full flex justify-center border border-border rounded-input pl-input-x mt-element-gap"
       aria-hidden="true"
     >
-      <div className="w-full flex justify-start items-center gap-element-gap rounded-input">
+      <div className="w-full flex justify-start items-center gap-element-gap rounded-input bg-background">
         <div className="h-10 flex justify-center items-center px-button-x">
           <div className="h-5 w-5 bg-border/40 rounded-full animate-pulse" />
         </div>
@@ -12,7 +12,7 @@ export default function SearchSkeleton() {
         <div className="flex justify-end items-center px-button-x">
           <div className="h-4 w-4 bg-border/40 rounded-full animate-pulse" />
         </div>
-        <div className="h-10 flex justify-center items-center px-2 rounded-r-input">
+        <div className="h-10 flex justify-center items-center px-button-x">
           <div className="h-5 w-5 bg-border/40 rounded-full animate-pulse" />
         </div>
       </div>
