63d56646f6f86863c116dce35fef42ee96bde706
Albert Olivé <albertolivecorbella@gmail.com>
2025-11-19 13:52:18 +0100

    chore: wip

--- components/ui/common/skeletons/index.ts
+++ components/ui/common/skeletons/index.ts
@@ -1,4 +1,3 @@
 export { default as EventCardSkeleton } from "./EventCardSkeleton";
 export { default as HomePageSkeleton } from "./HomePageSkeleton";
 export { default as PlacePageSkeleton } from "./PlacePageSkeleton";
-export { default as SearchSkeleton } from "./SearchSkeleton";
